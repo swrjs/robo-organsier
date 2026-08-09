b51c41d57c3c0a73a341639490a7fd5374ddefa9
swrjs <swarajsjimmy@gmail.com>
2026-08-10 02:32:10 +0530

    exclude frames  using .gitignore

--- frame_extractor.py
+++ frame_extractor.py
@@ -1,10 +1,12 @@
 import cv2
 import os
 
+os.makedirs(f"./frames", exist_ok=True)
+
 num=11
 while(num):
     vid=cv2.VideoCapture(f"./videos/{num}.MOV")
-    os.makedirs(f"./frames_video_{num}", exist_ok=True)
+    os.makedirs(f"./frames/frames_video_{num}", exist_ok=True)
 
     fps=vid.get(cv2.CAP_PROP_FPS)
     frame_interval = int(fps*0.5)
@@ -18,7 +20,7 @@ while(num):
 
         if current_frame_no % frame_interval == 0:
             print(f"Saving frame {current_frame_no} from video {num}...")
-            cv2.imwrite(f"./frames_video_{num}/video_{num}_frame_{current_frame_no}.jpg", current_frame)  
+            cv2.imwrite(f"./frames/frames_video_{num}/video_{num}_frame_{current_frame_no}.jpg", current_frame)  
 
         current_frame_no += 1
 
